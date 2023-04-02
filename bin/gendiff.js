16a41039a37dc09eb4c52bf1b39e2a3e8b970bdc
sinstranger <sinstranger@yandex.ru>
2023-04-02 17:35:04 +0300

    Step two

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
 import gendiff from "../src/gendiff.js";
-import { EOL } from "os";
 gendiff.parse(process.argv);
--- src/gendiff.js
+++ src/gendiff.js
@@ -3,6 +3,8 @@ const gendiff = new Command();
 gendiff
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 export default gendiff;
