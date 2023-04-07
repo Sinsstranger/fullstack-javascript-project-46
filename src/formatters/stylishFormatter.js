509cc3be21478f52feae3709abb6ff0c9d28a48f
sinstranger <sinstranger@yandex.ru>
2023-04-07 17:58:33 +0300

    Refactoring

--- src/formatters/stylishFormatter.js
+++ src/formatters/stylishFormatter.js
@@ -15,31 +15,29 @@ const stringify = (data, treeDepth) => {
   );
   return ["{", ...newObj, `${indent(treeDepth)}  }`].join(EOL);
 };
+const iter = (tree, depth) =>
+  tree.map((node) => {
+    const getValue = (value, sym) =>
+      `${indent(depth)}${sym} ${node.key}: ${stringify(value, depth)}${EOL}`;
+    switch (node.type) {
+      case "added":
+        return getValue(node.value, "+");
+      case "deleted":
+        return getValue(node.value, "-");
+      case "equal":
+        return getValue(node.value, " ");
+      case "updated":
+        return `${getValue(node.value1, "-")}${getValue(node.value2, "+")}`;
+      case "children":
+        return `${indent(depth)}  ${node.key}: {${EOL}${iter(
+          node.children,
+          depth + 1
+        ).join("")}${indent(depth)}  }${EOL}`;
+      default:
+        throw new Error("Wrong node Type!");
+    }
+  });
 
-const stylish = (innerTree) => {
-  const iter = (tree, depth) =>
-    tree.map((node) => {
-      const getValue = (value, sym) =>
-        `${indent(depth)}${sym} ${node.key}: ${stringify(value, depth)}${EOL}`;
-      switch (node.type) {
-        case "added":
-          return getValue(node.value, "+");
-        case "deleted":
-          return getValue(node.value, "-");
-        case "equal":
-          return getValue(node.value, " ");
-        case "updated":
-          return `${getValue(node.value1, "-")}${getValue(node.value2, "+")}`;
-        case "children":
-          return `${indent(depth)}  ${node.key}: {${EOL}${iter(
-            node.children,
-            depth + 1
-          ).join("")}${indent(depth)}  }${EOL}`;
-        default:
-          throw new Error("Wrong node Type!");
-      }
-    });
-  return `{${EOL}${iter(innerTree, 1).join("")}}`;
-};
+const stylish = (innerTree) => `{${EOL}${iter(innerTree, 1).join("")}}`;
 
 export default stylish;
