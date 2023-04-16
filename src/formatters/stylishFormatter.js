b013dead1fb7edb5a69528df9d37a5407977def0
sinstranger <sinstranger@yandex.ru>
2023-04-16 17:16:58 +0300

    stylish formatter fix

--- src/formatters/stylishFormatter.js
+++ src/formatters/stylishFormatter.js
@@ -1,51 +1,44 @@
 import _ from "lodash";
-import { EOL } from "os";
+
 const indent = (depth, spaceCount = 4) => " ".repeat(spaceCount * depth - 2);
 
 const stringify = (data, treeDepth) => {
-  if (!_.isObject(data)) {
-    return `${data}`;
-  }
-  if (data === null) {
-    return null;
-  }
+  if (!_.isObject(data)) return `${data}`;
+  if (data === null) return null;
   const newObj = Object.entries(data).map(
     ([key, value]) =>
       `${indent(treeDepth + 1)}  ${key}: ${stringify(value, treeDepth + 1)}`
   );
-  return ["{", ...newObj, `${indent(treeDepth)}  }`].join(EOL);
+  return ["{", ...newObj, `${indent(treeDepth)}  }`].join("\n");
 };
-const getValue = (key, value, sym, currentDepth) =>
-  `${indent(currentDepth)}${sym} ${key}: ${stringify(
-    value,
-    currentDepth
-  )}${EOL}`;
-const iter = (tree, depth) =>
-  tree.map((node) => {
-    switch (node.type) {
-      case "added":
-        return getValue(node.key, node.value, "+", depth);
-      case "deleted":
-        return getValue(node.key, node.value, "-", depth);
-      case "equal":
-        return getValue(node.key, node.value, " ", depth);
-      case "updated":
-        return `${getValue(node.key, node.dataOneValue, "-", depth)}${getValue(
-          node.key,
-          node.dataTwoValue,
-          "+",
-          depth
-        )}`;
-      case "children":
-        return `${indent(depth)}  ${node.key}: {${EOL}${iter(
-          node.children,
-          depth + 1
-        ).join("")}${indent(depth)}  }${EOL}`;
-      default:
-        throw new Error("Wrong node Type!");
-    }
-  });
 
-const stylish = (innerTree) => `{${EOL}${iter(innerTree, 1).join("")}}`;
+const stylish = (innerTree) => {
+  const iter = (tree, depth) =>
+    tree.map((node) => {
+      const getValue = (value, sign) =>
+        `${indent(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
+      switch (node.type) {
+        case "added":
+          return getValue(node.value, "+");
+        case "deleted":
+          return getValue(node.value, "-");
+        case "equal":
+          return getValue(node.value, " ");
+        case "updated":
+          return `${getValue(node.dataOneValue, "-")}${getValue(
+            node.dataTwoValue,
+            "+"
+          )}`;
+        case "children":
+          return `${indent(depth)}  ${node.key}: {\n${iter(
+            node.children,
+            depth + 1
+          ).join("")}${indent(depth)}  }\n`;
+        default:
+          throw new Error("Error");
+      }
+    });
+  return `{\n${iter(innerTree, 1).join("")}}`;
+};
 
 export default stylish;
